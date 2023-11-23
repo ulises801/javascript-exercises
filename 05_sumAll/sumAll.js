18ae4ce3673d0f79618d22d55cb58ee91458874f
ulises801 <71356367+ulises801@users.noreply.github.com>
2023-11-22 23:32:01 -0600

    feat: add 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    let temp = 0;
 
+    if(start>end){
+        temp = start;
+        start = end;
+        end = temp;
+    }else if(start<0){
+        return 'ERROR';
+    }else if (typeof start !== 'number' || typeof end !== 'number'){
+        return 'ERROR';
+    }else{
+
+    }
+
+    for(counter = start; counter <= end; counter++){
+        sum = sum+counter;
+    }
+    return sum;
 };
 
 // Do not edit below this line
