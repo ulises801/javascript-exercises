0f844b61e41215d8d59f82965a64e5f3246b4683
ulises801 <71356367+ulises801@users.noreply.github.com>
2023-11-22 23:02:01 -0600

    feat: add 04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayOfNumbers, ...numberToRemove) {
+    
+        for(let i=0; i < numberToRemove.length; i++){
+            for(let j = 0; j < arrayOfNumbers.length; j++){
+                if (numberToRemove[i] === arrayOfNumbers[j]){
+                    arrayOfNumbers.splice(j,1)
+                }
+            }
+        }
 
+        
+
+    return arrayOfNumbers;
 };
 
 // Do not edit below this line
