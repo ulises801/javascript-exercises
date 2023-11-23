aa06a155da404b0518b197e4a22d88c1bc312191
ulises801 <71356367+ulises801@users.noreply.github.com>
2023-11-23 00:35:42 -0600

    feat: add 06_leapYears solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let state;
+    if(year%100 == 0 && year%400 !== 0){
+        state = false;
+    }else if (year%4 == 0){
+        state = true;
+    }else {
+        state = false;
+    }
 
+    return state;
 };
 
 // Do not edit below this line
