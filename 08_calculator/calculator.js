bfd086654ad74c1ed76991b60da3c4b9113856bb
ulises801 <71356367+ulises801@users.noreply.github.com>
2024-01-23 02:52:05 -0600

    add feat: 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let count = 0;
+  if (arr.length === 0) {
+    return count;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      count = count + arr[i];
+    }
+    return count;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let count = 1;
+  for (let i = 0; i < arr.length; i++) {
+    count = count * arr[i];
+  }
+  return count;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 2;
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    for (let i = 3; i <= num; i++) {
+      fact = fact * i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +49,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
