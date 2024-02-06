8c6efd2769e3f0a99c6dac7bf6dc6d0d661c2b5c
ulises801 <71356367+ulises801@users.noreply.github.com>
2024-02-05 21:26:05 -0600

    feat add: 12_findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.sort(function (a, b) {
+    const lastGuy = getAge(a.yearOfDeath, a.yearOfBirth);
+    const nextGuy = getAge(b.yearOfDeath, b.yearOfBirth);
 
+    if (lastGuy > nextGuy) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return oldest[0];
+};
+
+const getAge = function (death, birth) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
 
 // Do not edit below this line
