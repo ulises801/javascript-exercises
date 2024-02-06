c85c0ab219d6118f736b1d0426d1cfe3d40e26eb
ulises801 <71356367+ulises801@users.noreply.github.com>
2024-02-05 20:08:21 -0600

    feat add: 10_fibonacci answer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (indice) {
+  let arr = [0, 1, 1];
 
+  for (let i = 3; i < 30; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+
+  if (indice < 0) {
+    return "OOPS";
+  }
+  return arr[indice];
 };
 
 // Do not edit below this line
